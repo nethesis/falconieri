fc999a66f2c4fe7347e65a2dd40a6c386892560c
Matteo Valentini <matteo.valentini@nethesis.it>
2019-10-10 17:26:47 +0200

    Fix typo in license header

--- models/mac.go
+++ models/mac.go
@@ -4,18 +4,18 @@
  *
  * This file is part of Falconieri project.
  *
- * Icaro is free software: you can redistribute it and/or modify
+ * Falconieri is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License,
  * or any later version.
  *
- * Icaro is distributed in the hope that it will be useful,
+ * Falconieri is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with Icaro.  If not, see LICENSE.
+ * along with Falconieri.  If not, see LICENSE.
  *
  * author: Matteo Valentini <matteo.valentini@nethesis.it>
  */
