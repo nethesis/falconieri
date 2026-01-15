d9c32373d0f2f184bfd28964dd360b9c6b512882
Matteo Valentini <matteo.valentini@nethesis.it>
2026-01-15 15:43:57 +0100

    feat(provider): add YMCS provider support
    Introduce initial YMCS provider integration, including configuration
    struct, environment variable parsing, and dispatch logic for device
    registration and optional PIN retrieval. Extend error parsing to handle
    YMCS-specific error formats and codes for invalid MAC and ownership
    conflicts.

---
+++ providers/ymcs.go
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2026 Nethesis S.r.l.
+ * http://www.nethesis.it - info@nethesis.it
+ *
+ * This file is part of Falconieri project.
+ *
+ * Falconieri is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or any later version.
+ *
+ * Falconieri is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Falconieri.  If not, see LICENSE.
+ *
+ * author: Matteo Valentini <matteo.valentini@nethesis.it>
+ */
+
+/*
+ * Yealink Management Cloud Service provider
+ */
+package providers
+
+import (
+	"errors"
+
+	"github.com/nethesis/falconieri/configuration"
+	"github.com/nethesis/falconieri/libs/ymcs"
+	"github.com/nethesis/falconieri/models"
+	"github.com/nethesis/falconieri/utils"
+)
+
+type YmcsDevice struct {
+	Mac string
+	Url string
+}
+
+func (s YmcsDevice) Register() error {
+	client := ymcs.NewClient(
+		configuration.Config.Providers.Ymcs.BaseURL,
+		configuration.Config.Providers.Ymcs.ClientID,
+		configuration.Config.Providers.Ymcs.ClientSecret,
+	)
+
+	// Delete device first (if it exists) to ensure clean registration
+	_, err := client.DeleteDeviceByMAC(s.Mac)
+	if err != nil {
+		// Ignore deletion errors (device may not exist)
+		// Log but continue with registration
+	}
+
+	resp, err := client.AddDeviceByMacSingle(s.Mac, &s.Url)
+	if err != nil {
+		return models.ProviderError{
+			Message:      "provider_remote_call_failed",
+			WrappedError: err,
+		}
+	}
+
+	if resp == nil {
+		return errors.New("unknown_response_from_provider")
+	}
+
+	if resp.FailureCount > 0 && len(resp.Errors) > 0 {
+		return utils.ParseProviderError(resp.Errors[0].ErrorInfo)
+	}
+
+	if resp.SuccessCount == 0 {
+		return errors.New("provider_remote_call_failed")
+	}
+
+	return nil
+}
+
+func (s YmcsDevice) GetPIN() (string, error) {
+	client := ymcs.NewClient(
+		configuration.Config.Providers.Ymcs.BaseURL,
+		configuration.Config.Providers.Ymcs.ClientID,
+		configuration.Config.Providers.Ymcs.ClientSecret,
+	)
+
+	return client.GetSingleDevicePIN(s.Mac)
+}
