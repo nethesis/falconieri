aa3e4017f8153e1793cbebac082f9da5f8e6feba
Matteo Valentini <matteo.valentini@nethesis.it>
2026-01-22 17:16:47 +0100

    feat(providers): add GRAPE provider
    Introduce new GRAPE provider support in system configuration and
    dispatch logic.

---
+++ providers/grape.go
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2026 Nethesis S.r.l.
+ * http://www.nethesis.it - info@nethesis.it
+ *
+ * This file is part of Falconieri project.
+ *
+ * Falconieri is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or any later version.
+ *
+ * Falconieri is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Falconieri.  If not, see LICENSE.
+ *
+ * author: Matteo Valentini <matteo.valentini@nethesis.it>
+ */
+
+/*
+ * Grape provisioning provider
+ */
+package providers
+
+import (
+	"errors"
+	"net"
+	"net/url"
+	"sync"
+
+	"github.com/nethesis/falconieri/configuration"
+	"github.com/nethesis/falconieri/libs/grape"
+	"github.com/nethesis/falconieri/models"
+)
+
+var (
+	grapeClient     *grape.Client
+	grapeClientOnce sync.Once
+)
+
+// getGrapeClient returns a singleton Grape client instance.
+// This allows API navigation caching to work effectively across multiple requests.
+// The client is created using configuration loaded at startup.
+func getGrapeClient() *grape.Client {
+	grapeClientOnce.Do(func() {
+		grapeClient = grape.NewClient(
+			configuration.Config.Providers.Grape.BaseURL,
+			configuration.Config.Providers.Grape.ClientID,
+			configuration.Config.Providers.Grape.ClientSecret,
+		)
+	})
+	return grapeClient
+}
+
+type GrapeDevice struct {
+	Mac string
+	Url string
+}
+
+func (d GrapeDevice) Register() error {
+	client := getGrapeClient()
+
+	err := client.RegisterDevice(d.Mac, d.Url)
+	if err != nil {
+		// Check if this is an API error (4xx/5xx from server)
+		var apiErr grape.APIError
+		if errors.As(err, &apiErr) {
+			// Return API error directly so caller can inspect status/message
+			return apiErr
+		}
+
+		// Check if this is a transport-level error (DNS, connection, timeout)
+		var urlErr *url.Error
+		if errors.As(err, &urlErr) {
+			return models.ProviderError{
+				Message:      "connection_to_remote_provider_failed",
+				WrappedError: err,
+			}
+		}
+
+		var netErr net.Error
+		if errors.As(err, &netErr) {
+			return models.ProviderError{
+				Message:      "connection_to_remote_provider_failed",
+				WrappedError: err,
+			}
+		}
+
+		// Other errors (JSON marshaling, etc.) - return as-is
+		return err
+	}
+
+	return nil
+}
